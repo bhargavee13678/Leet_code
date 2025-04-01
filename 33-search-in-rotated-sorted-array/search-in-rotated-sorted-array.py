33d993c7bb901230fd29c2d0cb78c0d9f2338a00
Bhargavee K Chavarkar <117475275+bhargavee13678@users.noreply.github.com>
2025-04-01 16:22:30 -0400

    Time: 0 ms (100.00%) | Memory: 12.7 MB (42.00%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -14,18 +14,18 @@ class Solution(object):
             if nums[mid] == target:
                 return mid
 
-            # Check if left half is sorted
+       
             if nums[left] <= nums[mid]:
-                # Check if target is in the left half
+   
                 if nums[left] <= target < nums[mid]:
                     right = mid - 1
                 else:
                     left = mid + 1
             else:
-                # Right half must be sorted
+         
                 if nums[mid] < target <= nums[right]:
                     left = mid + 1
                 else:
                     right = mid - 1
 
-        return -1  # Target not found
+        return -1  
