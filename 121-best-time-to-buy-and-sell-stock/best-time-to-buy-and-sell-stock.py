949195cf99b371728168c921755bdc9f71b95bac
Bhargavee K Chavarkar <117475275+bhargavee13678@users.noreply.github.com>
2026-08-17 00:15:37 -0400

    Time: 28 ms (86.99%) | Memory: 28.6 MB (75.73%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,18 +1,15 @@
-class Solution(object):
-    def maxProfit(self, prices):
-        """
-        :type prices: List[int]
-        :rtype: int
-        """
-        
-        min_price=float('inf')
-        max_profit=0
-        for price in prices:
-            if min_price> price:
-                min_price=price
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
 
-            profit= price-min_price
-            if profit > max_profit:
-                max_profit=profit
-        return max_profit
-        
+        min_price = float('inf')
+
+        max_profit = 0
+
+        for price in prices:
+            if price < min_price:
+                min_price = price
+
+            elif price - min_price > max_profit:
+                max_profit = price - min_price
+
+        return max_profit 
